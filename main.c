7b0b87007d7b05729ad5d2399314eb6888b0da5a
rascalia <radix.rascalia@gmail.com>
2025-10-16 10:30:37 +0700

    Feat: Add sapaDunia function.

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+void sapaDunia() {
+    printf("Hello, World from a function!\n");
+}
+
 int main(void) {
-    printf("Hello, World!\n");
+    printf("Ini adalah proyek konsol saya.\n");
+    sapaDunia(); // Memanggil fungsi baru
     return 0;
 }
